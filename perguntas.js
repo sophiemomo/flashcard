4a475717fb8f56d22c184169d7e6125692beffc1
sophiemomo <148099489+sophiemomo@users.noreply.github.com>
2024-10-28 18:46:11 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,5 @@
 criaCartao(
     'Pergunta',
-    'O que é',
-    'O Python é uma linguagem de programação'
+    '>O que é, o que é? Na água nasci, na água me criei, mas, se me jogarem na água, na água morrerei.',
+    'O sal.'
 )
